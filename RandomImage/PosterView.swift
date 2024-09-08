d6cc2b68549ea79c13a84f3575004979a0be95b7
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-08 17:21:37 +0900

    [Feat] url 결과값에 따른 case 분리

---
+++ RandomImage/PosterView.swift
@@ -0,0 +1,43 @@
+//
+//  PosterView.swift
+//  RandomImage
+//
+//  Created by 강석호 on 9/8/24.
+//
+
+import SwiftUI
+
+struct PosterView: View {
+    let url: String
+    var onImageSelected: (Image) -> Void
+    
+    var body: some View {
+        AsyncImage(url: URL(string: url)) { data in
+            switch data {
+            case .empty:
+                ProgressView()
+                    .frame(width: 100, height: 150)
+            case .success(let image):
+                image
+                    .resizable()
+                    .frame(width: 100, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .onTapGesture {
+                        onImageSelected(image)
+                    }
+            case .failure(_):
+                Image(systemName: "star")
+                    .frame(width: 100, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+            @unknown default:
+                Image(systemName: "star")
+                    .frame(width: 100, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+        }
+    }
+}
+
+#Preview {
+    PosterView(url: "https://picsum.photos/200/300") { _ in }
+}
