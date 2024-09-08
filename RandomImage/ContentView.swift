6e7e8782a7daf9a3279ddba9fcbb885a8fe8adcc
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-08 17:23:12 +0900

    [Feat] NavigationStack으로 navigationDestination 지정

--- RandomImage/ContentView.swift
+++ RandomImage/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let list = [
+        "첫번째 색션",
+        "두번째 색션",
+        "세번째 색션",
+        "네번째 색션"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView(.vertical) {
+                ForEach(list, id: \.self) { item in
+                    makeSection(item)
+                }
+                .padding(.leading)
+            }
+            .navigationTitle("My Random Image")
         }
-        .padding()
+    }
+    
+    func makeSection(_ title: String) -> some View {
+        VStack {
+            Text(title)
+                .padding(.top, 30)
+                .font(.headline)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            HorizontalSectionView()
+        }
     }
 }
 
