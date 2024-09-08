e1bb10415f29269142daeba51d8bdcc4bdf6f7eb
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-08 15:42:36 +0900

    Initial Commit

---
+++ RandomImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomImage
+//
+//  Created by 강석호 on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomImage/RandomImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageApp.swift
+//  RandomImage
+//
+//  Created by 강석호 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
